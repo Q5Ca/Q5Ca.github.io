191a8ce7898b54738652e70f0017ed7d32110c3a
Q5Ca <nghietsuc7@gmail.com>
2019-03-22 06:39:00 -0400

    add page-comp

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#content img").addClass("mx-auto d-block img-fluid mb-3");
+    $("#page-wrapper #post-content img").addClass("mx-auto d-block img-fluid mb-3");
     $('img').each(function(i){
         this.alt = encodeURI(this.src.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;') );
         });
